3e24874e8298156d7f88c81558049a43d7dfd719
Prem Chaitanya Prathi <chaitanyaprem@gmail.com>
2026-02-16 16:47:21 +0530

    fix delay and readme

--- waku/waku_mix/protocol.nim
+++ waku/waku_mix/protocol.nim
@@ -123,7 +123,7 @@ proc new*(
     peermgr.switch,
     spamProtection = Opt.some(SpamProtection(spamProtection)),
     delayStrategy =
-      ExponentialDelayStrategy.new(meanDelayMs = 50, rng = crypto.newRng()),
+      ExponentialDelayStrategy.new(meanDelayMs = 100, rng = crypto.newRng()),
   )
 
   processBootNodes(bootnodes, peermgr, m)
