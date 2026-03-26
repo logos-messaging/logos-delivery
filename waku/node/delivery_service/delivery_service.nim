108683dd85007f5ac8c5ec44049f9222d407f716
Fabiana Cecin <fabiana@waku.org>
2026-03-26 16:30:31 -0300

    fix lint again

--- waku/node/delivery_service/delivery_service.nim
+++ waku/node/delivery_service/delivery_service.nim
@@ -6,14 +6,9 @@ import
   ./recv_service,
   ./send_service,
   ./subscription_manager,
-  waku/
-    [
-      waku_core,
-      waku_node,
-      waku_store/client,
-      waku_relay/protocol,
-      waku_lightpush/client,
-    ]
+  waku/[
+    waku_core, waku_node, waku_store/client, waku_relay/protocol, waku_lightpush/client
+  ]
 
 type DeliveryService* = ref object
   sendService*: SendService
