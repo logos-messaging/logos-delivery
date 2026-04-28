b92a0ef26203ba77d6ec5d68fe8590f7f36324aa
darshankabariya <darshan@status.im>
2026-04-28 15:37:37 +0530

    feat: initial changes

--- waku/waku_rln_relay/constants.nim
+++ waku/waku_rln_relay/constants.nim
@@ -7,6 +7,8 @@ import ../waku_keystore
 # Acceptable roots for merkle root validation of incoming messages
 const AcceptableRootWindowSize* = 50
 
+const DummyConstant* = 50
+
 # RLN membership key and index files path
 const RlnCredentialsFilename* = "rlnCredentials.txt"
 
