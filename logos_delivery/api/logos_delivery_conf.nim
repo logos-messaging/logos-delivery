PearsonWhite/waku

Add Fleet mode and required infra/changes
* Rename WakuMode to LogosDeliveryMode, add Fleet mode
* KernelConf (which starts only kernel) is now a distinct WakuNodeConf (starts messaging, for tests)
* Add LogosDelivery.new(kernelConf) (kernel-only)
* Add LogosDelivery.new(kernelConf, messagingOverrides, channelsOverrides) (full stack)
* LogosDeliveryConf.messagingOverrides/channelsOverrides are Option and mount each layer if present
* Add kernel-only LogosDeliveryConf.init(kernelConf)
* Parse {"mode": "fleet", "kernelConf": {...}} in parseLogosDeliveryConf
* Generalize parseOverrides to parse over caller-supplied defaults
* Add requireMessaging/requireChannels FFI guards and apply them
* Make start/stop skip layers that were not mounted (no config)
* Rename toKernelConf to toWakuNodeConf
* Add tests to test_conf.nim
* Misc additions/refactors

logos_delivery/api/logos_delivery_conf.nim
@@ -1,30 +1,38 @@
 {.push raises: [].}
 
+import std/options
 import results
 
 import logos_delivery/api/messaging_conf
 import logos_delivery/channels/reliable_channel_manager
 
-export messaging_conf, reliable_channel_manager
+export options, messaging_conf, reliable_channel_manager
 
-type LogosDeliveryConf* = object ## Aggregates the per-layer config objects.
+type LogosDeliveryConf* = object
+  ## Aggregates the per-layer config objects. A layer is mounted iff its config
+  ## is present.
   kernelConf*: KernelConf
-  messaging*: MessagingClientConf
-  reliableChannel*: ReliableChannelManagerConf
+  messagingOverrides*: Option[MessagingClientConf]
+  channelsOverrides*: Option[ReliableChannelManagerConf]
+
+proc init*(T: type LogosDeliveryConf, kernelConf: KernelConf): LogosDeliveryConf =
+  return LogosDeliveryConf(kernelConf: kernelConf)
 
 proc init*(
     T: type LogosDeliveryConf,
-    mode: WakuMode,
+    mode: LogosDeliveryMode,
     preset: string,
     messagingOverrides: MessagingClientConf,
     channelsOverrides: ReliableChannelManagerConf,
 ): ConfResult[LogosDeliveryConf] =
   let merged = merge(?resolvePreset(preset), messagingOverrides)
-  var kernelConf = ?toKernelConf(merged, mode)
+  var kernelConf = ?toWakuNodeConf(merged, mode)
   kernelConf.preset = preset
   return ok(
     LogosDeliveryConf(
-      kernelConf: kernelConf, messaging: merged, reliableChannel: channelsOverrides
+      kernelConf: KernelConf(kernelConf),
+      messagingOverrides: some(merged),
+      channelsOverrides: some(channelsOverrides),
     )
   )
 

logos_delivery/logos_delivery.nim
@@ -75,19 +75,24 @@ type LogosDelivery* = ref object ## Entry point. Holds one instance of each API 
 proc new*(
     T: type LogosDelivery, conf: LogosDeliveryConf, appCallbacks: AppCallbacks = nil
 ): Future[Result[LogosDelivery, string]] {.async.} =
-  ## Builds the stack bottom-up from a resolved per-layer config.
-  let wakuConf = conf.kernelConf.toWakuConf().valueOr:
-    return err("failed to handle the configuration: " & error)
+  ## Builds the stack bottom-up from a resolved per-layer config; each layer is
+  ## mounted iff its config is present.
+  let wakuConf = WakuNodeConf(conf.kernelConf).toWakuConf().valueOr:
+      return err("failed to handle the configuration: " & error)
   let waku = (await Waku.new(wakuConf, appCallbacks)).valueOr:
     return err("failed to create Waku: " & error)
 
-  let messagingClient = MessagingClient.new(conf.messaging, waku).valueOr:
-    return err("failed to create MessagingClient: " & error)
+  var messagingClient: MessagingClient
+  if conf.messagingOverrides.isSome():
+    messagingClient = MessagingClient.new(conf.messagingOverrides.get(), waku).valueOr:
+      return err("failed to create MessagingClient: " & error)
 
-  let reliableChannelManager = ReliableChannelManager.new(
-    conf.reliableChannel, waku.brokerCtx
-  ).valueOr:
-    return err("failed to create ReliableChannelManager: " & error)
+  var reliableChannelManager: ReliableChannelManager
+  if conf.channelsOverrides.isSome():
+    reliableChannelManager = ReliableChannelManager.new(
+      conf.channelsOverrides.get(), waku.brokerCtx
+    ).valueOr:
+      return err("failed to create ReliableChannelManager: " & error)
 
   return ok(
     LogosDelivery(
@@ -103,16 +108,41 @@ proc new*(
   ## Builds the full stack from a kernel `WakuNodeConf`.
   return await LogosDelivery.new(
     LogosDeliveryConf(
-      kernelConf: conf,
-      messaging: MessagingClientConf(),
-      reliableChannel: ReliableChannelManagerConf(),
+      kernelConf: KernelConf(conf),
+      messagingOverrides: some(MessagingClientConf()),
+      channelsOverrides: some(ReliableChannelManagerConf()),
+    ),
+    appCallbacks,
+  )
+
+proc new*(
+    T: type LogosDelivery, kernelConf: KernelConf, appCallbacks: AppCallbacks = nil
+): Future[Result[LogosDelivery, string]] {.async.} =
+  ## Fleet mode: mounts the kernel only from a raw `KernelConf`; no messaging client,
+  ## no channel manager.
+  return await LogosDelivery.new(LogosDeliveryConf.init(kernelConf), appCallbacks)
+
+proc new*(
+    T: type LogosDelivery,
+    kernelConf: KernelConf,
+    messagingOverrides: MessagingClientConf,
+    channelsOverrides: ReliableChannelManagerConf,
+    appCallbacks: AppCallbacks = nil,
+): Future[Result[LogosDelivery, string]] {.async.} =
+  ## Full stack: kernel + messaging + channels. Messaging is never skipped; a
+  ## kernel-only node uses `new(kernelConf)` instead.
+  return await LogosDelivery.new(
+    LogosDeliveryConf(
+      kernelConf: kernelConf,
+      messagingOverrides: some(messagingOverrides),
+      channelsOverrides: some(channelsOverrides),
     ),
     appCallbacks,
   )
 
 proc new*(
     T: type LogosDelivery,
-    mode: WakuMode = WakuMode.Core,
+    mode: LogosDeliveryMode = LogosDeliveryMode.Core,
     preset: string = "",
     messagingOverrides: MessagingClientConf = MessagingClientConf(),
     channelsOverrides: ReliableChannelManagerConf = ReliableChannelManagerConf(),
@@ -124,22 +154,20 @@ proc new*(
   return await LogosDelivery.new(conf, appCallbacks)
 
 proc start*(self: LogosDelivery): Future[Result[void, string]] {.async.} =
-  ## Starts each layer bottom-up: transport first, then messaging, then channels.
+  ## Starts each present layer bottom-up: transport, then messaging, then channels.
   if self.waku.isNil():
     return err("Waku node is not initialized")
-  if self.messagingClient.isNil():
-    return err("MessagingClient is not initialized")
-  if self.reliableChannelManager.isNil():
-    return err("ReliableChannelManager is not initialized")
 
   (await self.waku.start()).isOkOr:
     return err("failed to start Waku: " & error)
 
-  self.messagingClient.start().isOkOr:
-    return err("failed to start MessagingClient: " & error)
+  if not self.messagingClient.isNil():
+    self.messagingClient.start().isOkOr:
+      return err("failed to start MessagingClient: " & error)
 
-  self.reliableChannelManager.start().isOkOr:
-    return err("failed to start ReliableChannelManager: " & error)
+  if not self.reliableChannelManager.isNil():
+    self.reliableChannelManager.start().isOkOr:
+      return err("failed to start ReliableChannelManager: " & error)
 
   return ok()
 

logos-messaging/logos-delivery

logos_delivery/api/logos_delivery_conf.nim
@@ -1,30 +1,38 @@
 {.push raises: [].}
 
+import std/options
 import results
 
 import logos_delivery/api/messaging_conf
 import logos_delivery/channels/reliable_channel_manager
 
-export messaging_conf, reliable_channel_manager
+export options, messaging_conf, reliable_channel_manager
 
-type LogosDeliveryConf* = object ## Aggregates the per-layer config objects.
+type LogosDeliveryConf* = object
+  ## Aggregates the per-layer config objects. A layer is mounted iff its config
+  ## is present.
   kernelConf*: KernelConf
-  messagingConf*: MessagingClientConf
-  channelsConf*: ReliableChannelManagerConf
+  messagingConf*: Option[MessagingClientConf]
+  channelsConf*: Option[ReliableChannelManagerConf]
+
+proc init*(T: type LogosDeliveryConf, kernelConf: KernelConf): LogosDeliveryConf =
+  return LogosDeliveryConf(kernelConf: kernelConf)
 
 proc init*(
     T: type LogosDeliveryConf,
-    mode: WakuMode,
+    mode: LogosDeliveryMode,
     preset: string,
     messagingOverrides: MessagingClientConf,
     channelsOverrides: ReliableChannelManagerConf,
 ): ConfResult[LogosDeliveryConf] =
   let merged = merge(?resolvePreset(preset), messagingOverrides)
-  var kernelConf = ?toKernelConf(merged, mode)
+  var kernelConf = ?toWakuNodeConf(merged, mode)
   kernelConf.preset = preset
   return ok(
     LogosDeliveryConf(
-      kernelConf: kernelConf, messagingConf: merged, channelsConf: channelsOverrides
+      kernelConf: KernelConf(kernelConf),
+      messagingConf: some(merged),
+      channelsConf: some(channelsOverrides),
     )
   )
 

logos_delivery/logos_delivery.nim
@@ -75,19 +75,24 @@ type LogosDelivery* = ref object ## Entry point. Holds one instance of each API 
 proc new*(
     T: type LogosDelivery, conf: LogosDeliveryConf, appCallbacks: AppCallbacks = nil
 ): Future[Result[LogosDelivery, string]] {.async.} =
-  ## Builds the stack bottom-up from a resolved per-layer config.
-  let wakuConf = conf.kernelConf.toWakuConf().valueOr:
-    return err("failed to handle the configuration: " & error)
+  ## Builds the stack bottom-up from a resolved per-layer config; each layer is
+  ## mounted iff its config is present.
+  let wakuConf = WakuNodeConf(conf.kernelConf).toWakuConf().valueOr:
+      return err("failed to handle the configuration: " & error)
   let waku = (await Waku.new(wakuConf, appCallbacks)).valueOr:
     return err("failed to create Waku: " & error)
 
-  let messagingClient = MessagingClient.new(conf.messagingConf, waku).valueOr:
-    return err("failed to create MessagingClient: " & error)
+  var messagingClient: MessagingClient
+  if conf.messagingConf.isSome():
+    messagingClient = MessagingClient.new(conf.messagingConf.get(), waku).valueOr:
+      return err("failed to create MessagingClient: " & error)
 
-  let reliableChannelManager = ReliableChannelManager.new(
-    conf.channelsConf, waku.brokerCtx
-  ).valueOr:
-    return err("failed to create ReliableChannelManager: " & error)
+  var reliableChannelManager: ReliableChannelManager
+  if conf.channelsConf.isSome():
+    reliableChannelManager = ReliableChannelManager.new(
+      conf.channelsConf.get(), waku.brokerCtx
+    ).valueOr:
+      return err("failed to create ReliableChannelManager: " & error)
 
   return ok(
     LogosDelivery(
@@ -103,16 +108,41 @@ proc new*(
   ## Builds the full stack from a kernel `WakuNodeConf`.
   return await LogosDelivery.new(
     LogosDeliveryConf(
-      kernelConf: conf,
-      messagingConf: MessagingClientConf(),
-      channelsConf: ReliableChannelManagerConf(),
+      kernelConf: KernelConf(conf),
+      messagingConf: some(MessagingClientConf()),
+      channelsConf: some(ReliableChannelManagerConf()),
+    ),
+    appCallbacks,
+  )
+
+proc new*(
+    T: type LogosDelivery, kernelConf: KernelConf, appCallbacks: AppCallbacks = nil
+): Future[Result[LogosDelivery, string]] {.async.} =
+  ## Fleet mode: mounts the kernel only from a raw `KernelConf`; no messaging client,
+  ## no channel manager.
+  return await LogosDelivery.new(LogosDeliveryConf.init(kernelConf), appCallbacks)
+
+proc new*(
+    T: type LogosDelivery,
+    kernelConf: KernelConf,
+    messagingOverrides: MessagingClientConf,
+    channelsOverrides: ReliableChannelManagerConf,
+    appCallbacks: AppCallbacks = nil,
+): Future[Result[LogosDelivery, string]] {.async.} =
+  ## Full stack: kernel + messaging + channels. Messaging is never skipped; a
+  ## kernel-only node uses `new(kernelConf)` instead.
+  return await LogosDelivery.new(
+    LogosDeliveryConf(
+      kernelConf: kernelConf,
+      messagingConf: some(messagingOverrides),
+      channelsConf: some(channelsOverrides),
     ),
     appCallbacks,
   )
 
 proc new*(
     T: type LogosDelivery,
-    mode: WakuMode = WakuMode.Core,
+    mode: LogosDeliveryMode = LogosDeliveryMode.Core,
     preset: string = "",
     messagingOverrides: MessagingClientConf = MessagingClientConf(),
     channelsOverrides: ReliableChannelManagerConf = ReliableChannelManagerConf(),
@@ -124,22 +154,20 @@ proc new*(
   return await LogosDelivery.new(conf, appCallbacks)
 
 proc start*(self: LogosDelivery): Future[Result[void, string]] {.async.} =
-  ## Starts each layer bottom-up: transport first, then messaging, then channels.
+  ## Starts each present layer bottom-up: transport, then messaging, then channels.
   if self.waku.isNil():
     return err("Waku node is not initialized")
-  if self.messagingClient.isNil():
-    return err("MessagingClient is not initialized")
-  if self.reliableChannelManager.isNil():
-    return err("ReliableChannelManager is not initialized")
 
   (await self.waku.start()).isOkOr:
     return err("failed to start Waku: " & error)
 
-  self.messagingClient.start().isOkOr:
-    return err("failed to start MessagingClient: " & error)
+  if not self.messagingClient.isNil():
+    self.messagingClient.start().isOkOr:
+      return err("failed to start MessagingClient: " & error)
 
-  self.reliableChannelManager.start().isOkOr:
-    return err("failed to start ReliableChannelManager: " & error)
+  if not self.reliableChannelManager.isNil():
+    self.reliableChannelManager.start().isOkOr:
+      return err("failed to start ReliableChannelManager: " & error)
 
   return ok()
 