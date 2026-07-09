PearsonWhite/waku

Fixes from Ivan's review
* messaging/channels: let/if-expr instead of var+mutate
* move the has-messaging/has-channels guard (ensureMessaging/ensureChannels)
* tests: require isSome before get

logos_delivery/logos_delivery.nim
@@ -82,17 +82,19 @@ proc new*(
   let waku = (await Waku.new(wakuConf, appCallbacks)).valueOr:
     return err("failed to create Waku: " & error)
 
-  var messagingClient: MessagingClient
-  if conf.messagingOverrides.isSome():
-    messagingClient = MessagingClient.new(conf.messagingOverrides.get(), waku).valueOr:
-      return err("failed to create MessagingClient: " & error)
+  let messagingClient =
+    if conf.messagingOverrides.isSome():
+      MessagingClient.new(conf.messagingOverrides.get(), waku).valueOr:
+        return err("failed to create MessagingClient: " & error)
+    else:
+      nil
 
-  var reliableChannelManager: ReliableChannelManager
-  if conf.channelsOverrides.isSome():
-    reliableChannelManager = ReliableChannelManager.new(
-      conf.channelsOverrides.get(), waku.brokerCtx
-    ).valueOr:
-      return err("failed to create ReliableChannelManager: " & error)
+  let reliableChannelManager =
+    if conf.channelsOverrides.isSome():
+      ReliableChannelManager.new(conf.channelsOverrides.get(), waku.brokerCtx).valueOr:
+        return err("failed to create ReliableChannelManager: " & error)
+    else:
+      nil
 
   return ok(
     LogosDelivery(
@@ -187,6 +189,18 @@ proc isOnline*(self: LogosDelivery): Future[Result[bool, string]] {.async.} =
   if self.waku.isNil():
     return err("Waku node is not initialized")
   return await self.waku.isOnline()
+
+proc ensureMessaging*(self: LogosDelivery): Result[void, string] =
+  ## Fails if the node has no messaging client (a kernel-only / fleet node).
+  if self.isNil() or self.messagingClient.isNil():
+    return err("node has no messaging client (kernel-only/fleet node)")
+  ok()
+
+proc ensureChannels*(self: LogosDelivery): Result[void, string] =
+  ## Fails if the node has no reliable channel manager (a kernel-only / fleet node).
+  if self.isNil() or self.reliableChannelManager.isNil():
+    return err("node has no reliable channel manager (kernel-only/fleet node)")
+  ok()
 
 # Compile-time check that each concrete type satisfies its API concept.
 static:

tests/api/test_conf.nim
@@ -132,6 +132,7 @@ suite "parseLogosDeliveryConf - JSON parsing":
       """{"mode": "Core", "messagingOverrides": {"clusterId": 7, "reliabilityEnabled": true}}"""
     ).valueOr:
       raiseAssert error
+    require lc.messagingOverrides.isSome()
     check:
       WakuNodeConf(lc.kernelConf).clusterId == some(7'u16) # kernel field
       lc.messagingOverrides.get().reliabilityEnabled == some(true) # messaging-only
@@ -139,6 +140,7 @@ suite "parseLogosDeliveryConf - JSON parsing":
   test "messaging overrides are recorded verbatim, unset fields left none":
     let lc = parseLogosDeliveryConf("""{"messagingOverrides": {"clusterId": 7}}""").valueOr:
       raiseAssert error
+    require lc.messagingOverrides.isSome()
     let overrides = lc.messagingOverrides.get()
     check:
       overrides.clusterId == some(7'u16) # the user's override, kept as given
@@ -148,6 +150,7 @@ suite "parseLogosDeliveryConf - JSON parsing":
     let lc = parseLogosDeliveryConf("""{"preset": "twn"}""").valueOr:
       raiseAssert error
     # the user set no reliability override, but the preset supplies one
+    require lc.messagingOverrides.isSome()
     check lc.messagingOverrides.get().reliabilityEnabled.isSome()
 
   test "channelsOverrides fold into the channel conf":
@@ -155,6 +158,7 @@ suite "parseLogosDeliveryConf - JSON parsing":
       """{"channelsOverrides": {"rateLimitEnabled": true, "sdsMaxRetransmissions": 9}}"""
     ).valueOr:
       raiseAssert error
+    require lc.channelsOverrides.isSome()
     let channels = lc.channelsOverrides.get()
     check:
       channels.rateLimitEnabled == some(true)
@@ -177,6 +181,7 @@ suite "parseLogosDeliveryConf - JSON parsing":
     ).valueOr:
       raiseAssert error
     let kc = WakuNodeConf(lc.kernelConf)
+    require lc.messagingOverrides.isSome()
     check:
       kc.clusterId == some(7'u16)
       lc.messagingOverrides.get().reliabilityEnabled == some(true) # messaging-only

logos-messaging/logos-delivery

logos_delivery/logos_delivery.nim
@@ -82,17 +82,19 @@ proc new*(
   let waku = (await Waku.new(wakuConf, appCallbacks)).valueOr:
     return err("failed to create Waku: " & error)
 
-  var messagingClient: MessagingClient
-  if conf.messagingConf.isSome():
-    messagingClient = MessagingClient.new(conf.messagingConf.get(), waku).valueOr:
-      return err("failed to create MessagingClient: " & error)
+  let messagingClient =
+    if conf.messagingConf.isSome():
+      MessagingClient.new(conf.messagingConf.get(), waku).valueOr:
+        return err("failed to create MessagingClient: " & error)
+    else:
+      nil
 
-  var reliableChannelManager: ReliableChannelManager
-  if conf.channelsConf.isSome():
-    reliableChannelManager = ReliableChannelManager.new(
-      conf.channelsConf.get(), waku.brokerCtx
-    ).valueOr:
-      return err("failed to create ReliableChannelManager: " & error)
+  let reliableChannelManager =
+    if conf.channelsConf.isSome():
+      ReliableChannelManager.new(conf.channelsConf.get(), waku.brokerCtx).valueOr:
+        return err("failed to create ReliableChannelManager: " & error)
+    else:
+      nil
 
   return ok(
     LogosDelivery(
@@ -187,6 +189,18 @@ proc isOnline*(self: LogosDelivery): Future[Result[bool, string]] {.async.} =
   if self.waku.isNil():
     return err("Waku node is not initialized")
   return await self.waku.isOnline()
+
+proc ensureMessaging*(self: LogosDelivery): Result[void, string] =
+  ## Fails if the node has no messaging client (a kernel-only / fleet node).
+  if self.isNil() or self.messagingClient.isNil():
+    return err("node has no messaging client (kernel-only/fleet node)")
+  ok()
+
+proc ensureChannels*(self: LogosDelivery): Result[void, string] =
+  ## Fails if the node has no reliable channel manager (a kernel-only / fleet node).
+  if self.isNil() or self.reliableChannelManager.isNil():
+    return err("node has no reliable channel manager (kernel-only/fleet node)")
+  ok()
 
 # Compile-time check that each concrete type satisfies its API concept.
 static:

tests/api/test_conf.nim
@@ -132,6 +132,7 @@ suite "parseLogosDeliveryConf - JSON parsing":
       """{"mode": "Core", "messagingOverrides": {"clusterId": 7, "reliabilityEnabled": true}}"""
     ).valueOr:
       raiseAssert error
+    require lc.messagingConf.isSome()
     check:
       WakuNodeConf(lc.kernelConf).clusterId == some(7'u16) # kernel field
       lc.messagingConf.get().reliabilityEnabled == some(true) # messaging-only
@@ -139,6 +140,7 @@ suite "parseLogosDeliveryConf - JSON parsing":
   test "messaging overrides are recorded verbatim, unset fields left none":
     let lc = parseLogosDeliveryConf("""{"messagingOverrides": {"clusterId": 7}}""").valueOr:
       raiseAssert error
+    require lc.messagingConf.isSome()
     let overrides = lc.messagingConf.get()
     check:
       overrides.clusterId == some(7'u16) # the user's override, kept as given
@@ -148,6 +150,7 @@ suite "parseLogosDeliveryConf - JSON parsing":
     let lc = parseLogosDeliveryConf("""{"preset": "twn"}""").valueOr:
       raiseAssert error
     # the user set no reliability override, but the preset supplies one
+    require lc.messagingConf.isSome()
     check lc.messagingConf.get().reliabilityEnabled.isSome()
 
   test "channelsOverrides fold into the channel conf":
@@ -155,6 +158,7 @@ suite "parseLogosDeliveryConf - JSON parsing":
       """{"channelsOverrides": {"rateLimitEnabled": true, "sdsMaxRetransmissions": 9}}"""
     ).valueOr:
       raiseAssert error
+    require lc.channelsConf.isSome()
     let channels = lc.channelsConf.get()
     check:
       channels.rateLimitEnabled == some(true)
@@ -177,6 +181,7 @@ suite "parseLogosDeliveryConf - JSON parsing":
     ).valueOr:
       raiseAssert error
     let kc = WakuNodeConf(lc.kernelConf)
+    require lc.messagingConf.isSome()
     check:
       kc.clusterId == some(7'u16)
       lc.messagingConf.get().reliabilityEnabled == some(true) # messaging-only