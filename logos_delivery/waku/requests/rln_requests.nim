39ce9ea8b62b5172c703ace0090a0a690850a060
stubbsta <tanya.steyn22@gmail.com>
2026-08-27 14:37:40 +0200

    Implementation of RLN-API module functions as alternative to group_manager/evm RLN

--- logos_delivery/waku/requests/rln_requests.nim
+++ logos_delivery/waku/requests/rln_requests.nim
@@ -1,5 +1,8 @@
 import brokers/request_broker
 import logos_delivery/waku/waku_core/message/message
+import logos_delivery/waku/rln/api/types as rln_api_types
+
+export rln_api_types
 
 RequestBroker:
   type RequestGenerateRlnProof* = object
@@ -8,3 +11,28 @@ RequestBroker:
   proc signature(
     message: WakuMessage, senderEpoch: float64
   ): Future[Result[RequestGenerateRlnProof, string]] {.async.}
+
+RequestBroker:
+  type RequestValidateRlnProof* = object
+    validation*: ValidationResult
+
+  proc signature(
+    message: WakuMessage,
+    registryId: RegistryId,
+    rlnIdentifier: RlnIdentifier,
+    timestamp: uint64,
+  ): Future[Result[RequestValidateRlnProof, string]] {.async.}
+
+RequestBroker:
+  type RequestStartRlnModule* = object
+    response*: string
+
+  proc signature(): Future[Result[RequestStartRlnModule, string]] {.async.}
+
+RequestBroker:
+  type RequestRegisterRlnMembership* = object
+    response*: string
+
+  proc signature(
+    registryId: RegistryId, rlnIdentifier: RlnIdentifier, options: RegistryOptions
+  ): Future[Result[RequestRegisterRlnMembership, string]] {.async.}
