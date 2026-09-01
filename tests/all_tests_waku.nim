fb42c615f5375027046c8d72171d7e7b6f709199
Richard Ramos <info@richardramos.me>
2026-09-01 09:12:47 -0400

    test(mix): cover Delivery Mix-RLN coordination

--- tests/all_tests_waku.nim
+++ tests/all_tests_waku.nim
@@ -62,7 +62,8 @@ import
   ./test_waku_rendezvous,
   ./test_waku_metadata,
   ./waku_discv5/test_waku_discv5,
-  ./waku_kademlia/test_waku_kademlia
+  ./waku_kademlia/test_waku_kademlia,
+  ./waku_mix/test_mix_rln_delivery
 
 # Waku Keystore test suite
 import ./test_waku_keystore_keyfile, ./test_waku_keystore
