f1a125cddad71239f245a48229c528ee123d24f0
Radosław Kamiński <radoslaw@status.im>
2026-09-10 12:10:47 +0100

    test: resolve the relay tests (migration 4) (#4222)

--- tests-e2e/tests/rest/relay/test_multiple_nodes.py
+++ tests-e2e/tests/rest/relay/test_multiple_nodes.py
@@ -7,3 +7,17 @@ class TestRelayMultipleNodes(StepsRelay):
     @pytest.mark.smoke
     def test_first_node_to_start_publishes(self, subscribe_optional_relay_nodes, relay_warm_up):
         self.check_published_message_reaches_relay_peer()
+
+    def test_last_node_to_start_publishes(self, subscribe_optional_relay_nodes, relay_warm_up):
+        self.check_published_message_reaches_relay_peer(sender=self.optional_nodes[-1])
+
+    def test_relay_get_message_while_one_peer_is_paused(self, subscribe_optional_relay_nodes, relay_warm_up):
+        self.check_published_message_reaches_relay_peer()
+        relay_message1 = self.create_message(contentTopic=self.test_content_topic)
+        relay_message2 = self.create_message(contentTopic=self.test_content_topic)
+        self.node2.pause()
+        self.node1.send_relay_message(relay_message1, self.test_pubsub_topic)
+        self.node2.unpause()
+        self.node1.send_relay_message(relay_message2, self.test_pubsub_topic)
+        messages = self.node2.get_relay_messages(self.test_pubsub_topic)
+        assert len(messages) == 2, "Both messages should've been returned"
--- tests/testlib/testasync.nim
+++ tests/testlib/testasync.nim
@@ -1,5 +1,8 @@
-# Sourced from: vendor/nim-libp2p/tests/testutils.nim
+# Sourced from: nim-libp2p/tests/tools/unittest.nim
 # Adds the ability for asyncSetup and asyncTeardown to be used in unittest2
+
+import std/macros
+import chronos, testutils/unittests
 
 template asyncTeardown*(body: untyped): untyped =
   teardown:
@@ -18,3 +21,55 @@ template asyncSetup*(body: untyped): untyped =
           body
       )()
     )
+
+const
+  timeoutDefault: Duration = 10.seconds
+  sleepIntervalDefault: Duration = 100.milliseconds
+
+proc buildAndExpr(n: NimNode): NimNode =
+  # Helper proc to recursively build a combined boolean expression
+
+  if n.kind == nnkStmtList and n.len > 0:
+    var combinedExpr = n[0] # Start with the first expression
+    for i in 1 ..< n.len:
+      # Combine the current expression with the next using 'and'
+      combinedExpr = newCall("and", combinedExpr, n[i])
+    return combinedExpr
+  else:
+    return n
+
+macro checkUntilTimeoutCustom*(
+    timeout: Duration, sleepInterval: Duration, code: untyped
+): untyped =
+  ## Periodically checks a given condition until it is true or a timeout occurs.
+  ##
+  ## `code`: untyped - A condition expression that should eventually evaluate to true.
+  ## `timeout`: Duration - The maximum duration to wait for the condition to be true.
+
+  # Build the combined expression
+  let combinedBoolExpr = buildAndExpr(code)
+
+  quote:
+    proc checkExpiringInternal(): Future[void] {.gensym, async.} =
+      let start = Moment.now()
+      while true:
+        if Moment.now() > (start + `timeout`):
+          checkpoint(
+            "[TIMEOUT] Timeout was reached and the conditions were not true. Check if the code is working as " &
+              "expected or consider increasing the timeout param."
+          )
+          check `code`
+          return
+        else:
+          if `combinedBoolExpr`:
+            return
+          else:
+            await sleepAsync(`sleepInterval`)
+
+    await checkExpiringInternal()
+
+macro checkUntilTimeout*(code: untyped): untyped =
+  ## Same as `checkUntilTimeoutCustom` but with a default timeout of 10s with 100ms
+  ## interval.
+  quote:
+    checkUntilTimeoutCustom(timeoutDefault, sleepIntervalDefault, `code`)
